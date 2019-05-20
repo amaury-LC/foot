df4f98d9b63d7f6bc7ef40ccbf790762e95a2f72
amaury-LC <Amaury.le.chaffotec@laposte.net>
2019-05-20 17:05:30 +0200

    boostrap3

--- index.php
+++ index.php
@@ -14,20 +14,14 @@
 
 		<div class="row" style="margin-top: 200px;">
 
-			<div class="col-lg-8" style="height: 156px;padding: 5px ;margin-bottom: 5px;">
+			<div class="col-lg-8" style="height: 156px;padding: 0px;margin: 0px;background: #0AFE06 ">
 
-				<div style="width: 100%;height: 156px;background: red">
-					
-
-				</div>
+				
 
 			</div>
-			<div class="col-lg-4" style="height: 156px;padding: 5px ;margin-bottom: 5px; ">
+			<div class="col-lg-4" style="height: 156px;padding: 0px;margin: 0px;background: blue">
 
-				<div style="width: 100%;height: 156px;background: blue">
-					
-
-				</div>
+				
 
 			</div>
 			
@@ -36,41 +30,23 @@
 
 		<div class="row" >
 
-			<div class="col-lg-8" style="height: 312px;padding: 5px;margin-bottom: 5px; ">
+			<div class="col-lg-8" style="height: 312px;padding: 0px;margin: 0px ;background: red">
 
-				<div style="width: 100%;height: 312px;background: red">
-					
-
-				</div>
+				
 
 			</div>
-			<div class="col-lg-4" style=";height: 312px;  ">
+			<div class="col-lg-4" style=";height: 312px; padding: 0px;margin: 0px;   ">
 
-				<div class=".container-fluid">
+				<div class="container">
 
-					<div class="row">
+				<div class="row" >
 
-						<div class="col-12" style="height: 156px;padding: 5px;margin-bottom: 5px; ">
-
-							<div style="width: 100%;height: 156px;background: yellow">
-
-
-							</div>
-
-							
-
-						</div>
-						<div class="col-12" style="height: 156px;padding: 5px ;margin-bottom: 5px;">
-
-							<div style="width: 100%;height: 156px;background: #FE8206 ">
-
-
-							</div>
-							
-
-						</div>
+					<div class="col-12" style="height: 156px;background: #FEBE06">
 						
 
+					</div>
+					<div class="col-12" style="height: 156px;background: yellow">
+						
 
 					</div>
 					
@@ -78,8 +54,50 @@
 				</div>
 
 			</div>
+
+				
+
+					
+
+						
+
+
+					
+					
+
+				
+
+			</div>
 			
 			
+
+		</div>
+
+
+		<!-- 2 partie -->
+
+		<div class="row">
+
+			<div class="col-lg-4" style="background: #0AFE06;height: 156px;" >
+
+				
+				
+
+
+			</div>
+			<div class="col-lg-8" style="background: #FE06FA;height: 156px">
+ 
+				
+				
+
+
+			</div>
+
+
+			
+
+
+
 
 		</div>
 		
@@ -91,11 +109,12 @@
 	
 
 
-	<script src="script/jquery.js" type="text/javascript"></script>
-	<script src="script/background.js" type="text/javascript"></script>	
-	<script type="text/javascript" src="script.js"></script>
+	
+	
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="script/jquerry.js" type="text/javascript"></script>
+	<script src="script/background.js" type="text/javascript"></script>	
 </body>
 </html>
