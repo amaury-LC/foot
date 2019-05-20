fe94f47b07687b8e78a3eb6616c9e55a4671a33c
amaury-LC <Amaury.le.chaffotec@laposte.net>
2019-05-20 14:13:12 +0200

    boostrap

--- index.php
+++ index.php
@@ -3,10 +3,42 @@
 <head>
 	<title>foot</title>
 	<link rel="stylesheet" type="text/css" href="style.css" media="screen">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 <body>
 
-	<img src="img/stade3.jpg" class="superbg" />
+	<img src="img/stadeb.jpg" class="superbg" />
+
+	<div class="container" >
+
+		<div class="row" style="margin-top: 200px;">
+
+			<div class="col-lg-8" style="background: red;height: 156px; margin: 0px;margin-top :10px; ">
+
+			</div>
+			<div class="col-lg-4" style="background: blue;height: 156px;margin: 0px;margin-top :10px; ">
+
+			</div>
+			
+
+		</div>
+
+		<div class="row" >
+
+			<div class="col-lg-8" style="background: red;height: 312px; margin: 0px;margin-top :10px; ">
+
+			</div>
+			<div class="col-lg-4" style="background: blue;height: 312px;margin: 0px;margin-top :10px; ">
+
+			</div>
+			
+			
+
+		</div>
+		
+
+	</div>
 	
 
 
@@ -16,5 +48,8 @@
 	<script src="script/jquery.js" type="text/javascript"></script>
 	<script src="script/background.js" type="text/javascript"></script>	
 	<script type="text/javascript" src="script.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
