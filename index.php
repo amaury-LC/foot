8ef101ee49616f1ee2769fab1d7bbf1e9778c2d1
amaury-LC <Amaury.le.chaffotec@laposte.net>
2019-05-20 11:14:53 +0200

    debut projet foot

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>foot</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	
+
+
+
+<script type="text/javascript" src="script.js"></script>
+</body>
+</html>
