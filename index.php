03341d7a127d283c856c20b3fb7891753ad510f9
amaury-LC <Amaury.le.chaffotec@laposte.net>
2019-05-23 14:33:26 +0200

    text terminé

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
 
 			<div class="col-lg-8  v1" style="padding: 0px;padding-right: 5px">
 
+				<h3 class="apparition">Découvrez les nouveaux maillots 19/20</h3>
+				<p class="apparition1">Trouvez l’article aux couleurs de l’OL qui vous convient en <br> quelques clics parmi de nombreuses références !</p>
+
 				<div class="rubrique">
 
 					<div class="rubrique_1">
@@ -38,9 +41,15 @@
 				
 
 			</div>
-			<div class="col-lg-4"  style="padding: 0px">
+			<div class="col-lg-4 v10"  style="padding: 0px">
+
+				
+
 
 				<div class="rubrique8">
+
+					<h3 class="apparition2">Réservez vos places et abonnements</h3>
+					<p class="apparition1_2">Assistez aux matches de<br> l’Olympique Lyonnais !</p>
 
 					<div class="rubrique_1">
 
@@ -77,11 +86,18 @@
 
 			<div class="col-lg-8  v" style="padding: 0px;margin: 0px ;padding-right: 5px">
 
+				<h3 class="apparition">Suivez les actualités de <br> l'Olympique Lyonnais</h3>
+				<p class="apparition1">Retrouvez toutes les actualités des équipes professionnelles,<br> de l’Academy et du club, l'info en temps réel et en images ! <br> Découvrez également des contenus exclusifs et les événements <br> à ne pas manquer.</p>
+
+
+
 				
 
 				<div class="rubrique2">
 
 					<div class="rubrique2_2">
+
+
 
 
 					</div>
@@ -92,6 +108,9 @@
 
 
 				</div>
+
+
+
 				
 
 				
@@ -107,6 +126,9 @@
 
 
 							<div class="rubrique7">
+
+								<h3 class="apparition2_7">Abonnez-vous à la chaîne<br> officielle de l'OL</h3>
+								<p class="apparition1_2_7">Ne manquez pas vos émissions,<br> reportages et lives pour tout savoir sur<br> l’Olympique Lyonnais.</p>
 
 								<div class="rubrique_1">
 
@@ -131,9 +153,14 @@
 
 
 						</div>
-						<div class="col-12" style="padding: 0px; ">
+						<div class="col-12" style="padding: 0px;">
+
+							
 
 							<div class="rubrique6">
+
+								<h3 class="apparition2_6">Partagez un instant<br> inoubliable</h3>
+								<p class="apparition1_2_6">OL le Musée, Visite de Stade, Escape Game…</p>
 
 								<div class="rubrique_1">
 
@@ -191,6 +218,9 @@
 
 				<div class="rubrique4" >
 
+					<h3 class="apparition2_3">Rejoignez le programme de<br> fidélité MYOL</h3>
+					<p class="apparition1_2_3">Profitez d'avantages exclusifs !</p>
+
 					<div class="rubrique_1">
 
 						<div class="rubrique_2">
@@ -221,6 +251,9 @@
 			<div class="col-lg-8" style="padding : 0px">
 
 				<div class="rubrique3">
+
+					<h3 class="apparition2_3">Découvrez toutes les offres destinées aux entreprises</h3>
+					<p class="apparition1_2_3">Billets de match, abonnements, événements d’entreprise… Rejoignez<br> la communauté OL Business Team !</p>
 
 					<div class="rubrique_1">
 
@@ -263,6 +296,9 @@
 			<div class="col-lg-4" style="padding: 0px;padding-right: 5px">
 
 				<div  class="rubrique5">
+
+					<h3 class="apparition2_3">Suivez les actualités<br> d'OL Fondation</h3>
+					<p class="apparition1_2_3">Découvrez tous les engagements<br> solidaires et les nombreux événements.</p>
 
 					<div class="rubrique_1">
 
@@ -358,6 +394,9 @@
 			<div class="col-lg-4" style="padding: 0px;padding-left: 5px;">
 
 				<div class="rubrique9">
+
+					<h3 class="apparition2_3">Comment venir au stade ?</h3>
+					<p class="apparition1_2_3">Retrouvez les différents moyens<br> d'accès pour vous rendre au<br> Groupama Stadium.</p>
 
 					<div class ="rubrique_1">
 
